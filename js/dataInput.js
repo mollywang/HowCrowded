8284bcbf014b83100a7fbe802d620117239e947d
mollywang <mollywang1@gmail.com>
2016-04-18 12:23:57 -0400

    input data

--- js/dataInput.js
+++ js/dataInput.js
@@ -4,16 +4,88 @@ var Parse = cloud.Parse;
 Parse.initialize("EADFqiDJ5SS1qXkcJM6FILbpT9d14sO5gO3FwksD",
 				 "bdY6CgnZI7bNTxFWoepER874qK81WYLYUT62xSVp");
 
-var Response = Parse.Object.extend("Response");
-var test = new Response();
+var Response = Parse.Object.extend("Response"); //create template for Response class
 
-test.set("Answer", true);
-test.set("Place", "Blarney");
-test.set("Time", 23);
+//set all busy times @ blarneys
+for (i = 1; i < 3; i++) { 
+    var blarney = new Response();
+    blarney.set("Answer", true);
+	blarney.set("Place", "Blarneys");
+	blarney.set("Time", i);
 
 
-test.save(null, {
-	success: function(gameScore) {
-		console.log("Saved");
-	}
-});
+	blarney.save(null, {
+		success: function(gameScore) {
+			console.log("Saved");
+		}
+	});
+}
+
+for (i = 22; i < 25; i++) { 
+    var blarney = new Response();
+    blarney.set("Answer", true);
+	blarney.set("Place", "Blarneys");
+	blarney.set("Time", i);
+
+
+	blarney.save(null, {
+		success: function(gameScore) {
+			console.log("Saved");
+		}
+	});
+}
+ 
+//set all non-busy times @blarneys
+for (i = 3; i < 22; i++) { 
+    var blarney = new Response();
+    blarney.set("Answer", false);
+	blarney.set("Place", "Blarneys");
+	blarney.set("Time", i);
+
+
+	blarney.save(null, {
+		success: function(gameScore) {
+			console.log("Saved");
+		}
+	});
+}
+
+//set all busy times @ huntsman
+//set all non-busy times @ huntsman
+for (i = 9; i < 25; i++) { 
+    var huntsman = new Response();
+   	huntsman.set("Answer", true);
+	huntsman.set("Place", "Huntsman");
+	huntsman.set("Time", i);
+	huntsman.save(null, {
+		success: function(gameScore) {
+			console.log("Saved");
+		}
+	});
+}
+for (i = 1; i < 3; i++) { 
+    var huntsman = new Response();
+   	huntsman.set("Answer", true);
+	huntsman.set("Place", "Huntsman");
+	huntsman.set("Time", i);
+	huntsman.save(null, {
+		success: function(gameScore) {
+			console.log("Saved");
+		}
+	});
+}
+
+//set all non-busy times @ huntsman
+for (i = 3; i < 9; i++) { 
+    var huntsman = new Response();
+   	huntsman.set("Answer", false);
+	huntsman.set("Place", "Huntsman");
+	huntsman.set("Time", i);
+	huntsman.save(null, {
+		success: function(gameScore) {
+			console.log("Saved");
+		}
+	});
+}
+
+
