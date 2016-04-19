3eae4a84af0bb61294a0e8f796166dd59fc36836
Max Tromanhauser <mtromanhauser@gmail.com>
2016-04-18 21:12:01 -0400

    server response

--- js/server.js
+++ js/server.js
@@ -10,38 +10,20 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
-    //notify.send('+18577568635', 'Hey, b.', function(err, msg) {
-        //console.log(err);
-        //console.log(msg);
-        //res.send('Texting now.');
-    //});
     res.send('Under Construction');
 });
 
 
 // route to respond to a text message
 app.get('/response_hook', function(req, res) {
-    // validate that this request really came from Twilio
-    if (twilio.validateExpressRequest(req, creds.AUTH)) {
 
-        // ask for a response message
-        var response = switchboard.direct(res['From'], res['Body']);
+    var from = res.req.query.From;
+    var body = res.req.query.Body;
 
-        console.log('RECIEVED RESPONSE');
-        console.log(res);
-        console.log();
-        
-        if (response != null) {
-            notify.send(res['From'], response);
-        }
+    // ask for a response message
+    var response = switchboard.direct(from, body);
 
-        // send HTTP 200 Success response to Twilio
-        res.send(200);
-
-    }
-    else {
-        res.send(401);
-    }
+    res.send(response);
 });
 
 app.listen(app.get('port'), function() {
