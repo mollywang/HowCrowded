6e0025c28ca0e5064639fb4fc5a678b9be37a197
Alex Whitaker <alexwhitaker94@gmail.com>
2016-04-18 21:36:17 -0400

    Got aggregation to work

--- js/aggregation.js
+++ js/aggregation.js
@@ -1,21 +1,53 @@
 // returns current decision and confidence score for the given responses (res)
 
-var score = function(res) {
-	if (res.length == 0) {
-		return true, 0;
-	}
-	var total = 0; 
+var db = require('./db.js')();
 
-	// finding average from array of responses
-	for (var i = 0; i < res.length; i++) {
-		total += res[i];
-	}
+module.exports = function() {
 
-	var avg = total / res.length;
-	if (avg > 0.5) {
-		return true, (avg * 100);
-	}
-	else {
-		return false, ((1 - avg) * 100);
-	}
+    // initialize any db shit here
+    var cloud = require("parse-cloud-express");
+    var Parse = cloud.Parse;
+
+    Parse.initialize("EADFqiDJ5SS1qXkcJM6FILbpT9d14sO5gO3FwksD",
+                 "bdY6CgnZI7bNTxFWoepER874qK81WYLYUT62xSVp");
+    
+    return {
+        score: {
+        	getScore: function(place, time, callback) {
+        		var query = new Parse.Query("Response");
+        		query.descending("createdAt").find({
+        			success: function(results) {
+        				var yes = 0;
+        				var no = 0;
+        				for (var i = 0; i < results.length; i++) {
+        					if (results[i].get("Place") === place && results[i].get("Time") == time) {
+        						if (results[i].get("Answer") == true) yes++;
+        						else no++;
+        					}
+        				}
+
+        				var avg =  yes / (yes + no);
+						if (avg > 0.5) {
+							callback({
+								crowded: true, 
+								value: (avg * 100)
+							});
+						}
+						else {
+							callback({
+								crowded: false, 
+								value: (1 - avg) * 100
+							});
+						}
+
+        			},
+        			error: function(error) {
+        				console.log("Failed to aggregate data");
+        			}
+        		})
+        	}
+
+        }
+    }
 }
+
